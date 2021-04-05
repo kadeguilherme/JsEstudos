5790982378c0e1a7327e251c00af100179a53fea
Guilherme Aguiar <kadeguilherme@gmail.com>
2021-04-05 14:33:55 -0300

    Esqueleto da pagina

---
+++ page-login/src/pages/login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './login.css';
+
+const Login = () => {
+    return (
+        <div className='login'>
+            <div className='login-left'>
+                <h1>Digital</h1>
+            </div>
+            
+            <div className = 'login-right'>
+                <h1>Imagem</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
