077610b1c5b0fb57eb11dac1388d70af5cb0a987
Guilherme Aguiar <kadeguilherme@gmail.com>
2021-04-05 15:03:17 -0300

    Estilizando os texto dos heards

--- page-login/src/pages/login.js
+++ page-login/src/pages/login.js
@@ -6,9 +6,20 @@ const Login = () => {
         <div className='login'>
             <div className='login-left'>
                 <h1>Digital</h1>
+
+                <div id='login-phrase'>
+                    <h1>
+                        Inteligência Artificial Dirigindo
+                        Resultados para a indústria de viagens
+                </h1>
+                </div>
+
+                <p>
+                Bem vindo de volta! Por favor acesse sua conta.
+                </p>
             </div>
-            
-            <div className = 'login-right'>
+
+            <div className='login-right'>
                 <h1>Imagem</h1>
             </div>
         </div>
